d59067591c19e3aea570b9ec0302af47b6f4004e
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-04-29 07:18:28 -0400

    added movie view and cards

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MainView } from './components/main-view/main-view';
 import './index.scss';
 
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="myMovie">
-        <div>Good Morning!</div>
-      </div>
+      <MainView/>
     );
   }
 }
