6bd3b89e56cece8a1c414391b3ee15b2835a41e2
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-04-29 13:56:17 -0400

    adding axios connection

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,37 +6,25 @@ import { MovieView } from '../movie-view/movie-view';
 
 const dkLogo = require('../../img/dk.jpg');
 const srLogo = require('../../img/sr.jpg');
-const gbeLogo = require('../../img/gbu.jpeg')
+const gbeLogo = require('../../img/gbu.jpeg');
 
 export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'The Dark Knight',
-          Description:
-            'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-          ImagePath: dkLogo,
-        },
-        {
-          _id: 2,
-          Title: 'The Shawshank Redemption',
-          Description:
-            'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-          ImagePath: srLogo,
-        },
-        {
-          _id: 3,
-          Title: 'The Good, the Bad and the Ugly',
-          Description:
-            'A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.',
-          ImagePath: gbeLogo,
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
+  }
+  componentDidMount() {
+    axios
+      .get('https://whatdoiwatch.herokuapp.com/movies')
+      .then((response) => {
+        this.setState({ movies: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
