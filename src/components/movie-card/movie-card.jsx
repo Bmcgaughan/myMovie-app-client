ff0cef7738591f1c836bd11f19588f35d83316c3
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-05-02 07:09:24 -0400

    updates to MovieCard Props

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -18,11 +18,16 @@ export class MovieCard extends React.Component {
   }
 }
 
+//setting up default values for the MovieCard properties
+//ensuring values are strings and required
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string,
-    Description: PropTypes.string,
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
+      Name: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
       Name: PropTypes.string,
     }),
   }).isRequired,
