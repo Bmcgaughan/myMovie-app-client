af1e43161c88f86edfb4475a8aa704a81a46d49d
Bmcgaughan <87346945+Bmcgaughan@users.noreply.github.com>
2022-05-22 14:24:58 -0400

    added redux

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Container from 'react-bootstrap/Container';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import moviesApp from './reducers/reducers';
+
 import { MainView } from './components/main-view/main-view';
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (
+      <Provider store={store}>
+        <MainView />
+      </Provider>
+    );
   }
 }
 
